7ff9e4d0a91969bb33e4d144fa7cbd180d51dec1
mrn <10130438+mrn@users.noreply.github.com>
2022-02-22 09:38:35 +0100

    Fix global invite command; remove use limit; change response

--- commands/invite.js
+++ commands/invite.js
@@ -1,45 +1,48 @@
 const config = require('../config.json');
-const inviteChannelID = config.inviteChannelID;
 const responses = require('../data/responses.json');
 const parseString = require('../modules/parseString.js');
 
 module.exports.run = async (message, args) => {
-    console.log('Invite channel ID: ' + inviteChannelID);
-    const inviteChannel = await message.guild.channels.cache.get(inviteChannelID);
+    console.log('Invite channel ID: ' + config.inviteChannelID);
+    const inviteChannel = await message.guild.channels.cache.get(config.inviteChannelID);
     let expiration = 86400; // expiration time in seconds (24 hours = 86400 seconds)
-    let uses = 20;
+    // let uses = 20;
     let askedPolitely = false;
 
     if (args[0] === 'pls' || args[0] === 'please') {
         askedPolitely = true;
         expiration *= 2;
-        uses *= 2;
+        // uses *= 2;
     }
 
-    const invite = await inviteChannel.createInvite({
-        maxAge: expiration,
-        unique: true,
-        maxUses: uses,
-    }).catch(console.error);
+    try {
+        if (inviteChannel) { // if successful
+            const invite = await inviteChannel.createInvite({
+                maxAge: expiration,
+                unique: true,
+                // maxUses: uses,
+            });
 
-    if (invite) { // if successful
-        let msg = parseString.formatVariables(
-            responses.invite.link, [invite, uses]);
-        if (askedPolitely) {
-            msg += '\n' + parseString.formatVariables(
-            responses.invite.askedPolitely, [expiration / 3600, uses]);
+            let msg = parseString.formatVariables(
+                responses.invite.link, [invite]);
+            if (askedPolitely) {
+                msg += '\n' + parseString.formatVariables(
+                responses.invite.askedPolitely, [expiration / 3600]);
+            }
+            message.reply(msg);
         }
-        message.reply(msg);
-    }
-    else {
-        const err_msg = responses.invite.cannotCreate + ' ' + parseString.formatVariables(
-                responses.error.tagStaff, []);
-        message.channel.send(err_msg);
+        else {
+            const err_msg = responses.invite.cannotCreate + ' ' + parseString.formatVariables(
+                    responses.error.tagStaff, []);
+            message.channel.send(err_msg);
+        }
+    } catch (error) {
+        console.log(error)
     }
 };
 
 module.exports.config = {
     name: 'invite',
     description: 'Send server invite link in chat',
-    aliases: ['i', 'inv', 'invitelink', 'link'],
+    aliases: ['i', 'inv', 'link', 'invitelink'],
 };
